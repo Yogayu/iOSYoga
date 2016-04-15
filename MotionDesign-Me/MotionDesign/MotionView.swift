8d95a673258166ce36e8b353784c0b0cc53cd2be
youxinyu <1015701481@qq.com>
2016-04-15 23:59:17 +0800

    new

--- MotionDesign-Me/MotionDesign/MotionView.swift
+++ MotionDesign-Me/MotionDesign/MotionView.swift
@@ -14,6 +14,8 @@ class MotionView:UIView {
     
     var view = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
     var redBall = UIView(frame: CGRect(x: 50, y: 50, width: 100, height: 100))
+
+    
 //    redBall.backgroundColor = UIColor.redColor()
 //    redBall.layer.cornerRadius = 50
 //    view.addSubview(redBall)
--- MotionDesign-Me/MotionDesign/ViewController.swift
+++ MotionDesign-Me/MotionDesign/ViewController.swift
@@ -8,29 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let screenRect = UIScreen.mainScreen().bounds
+let y_aix = 2/3 * screenRect.height
 
+class ViewController: UIViewController {
+    
+    let redBall = UIView(frame: CGRect(x: 0, y: y_aix, width: 20, height: 20))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
+        self.view.addSubview(redBall)
+        self.redBall.backgroundColor = UIColor.redColor()
+        self.redBall.layer.cornerRadius = 10
+        self.redBall.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    func moveBall() {
+        let pointStart:CGPoint = CGPoint(x: screenRect.width-10, y: y_aix)
+        let pointEnd:CGPoint = CGPoint(x: 10, y: y_aix)
+        print("where are you come from?")
+        UIView.animateWithDuration(1, delay: 0, usingSpringWithDamping: 1.0, initialSpringVelocity: 0, options: UIViewAnimationOptions.CurveLinear , animations: { self.redBall.center = pointStart},completion: { (true) in
+            UIView.animateWithDuration(1, delay: 0, usingSpringWithDamping: 1.0, initialSpringVelocity: 0, options: UIViewAnimationOptions.CurveLinear , animations: {self.redBall.center = pointEnd},completion: nil)
+        })
+    }
     @IBAction func buttonDidTouched(sender: AnyObject) {
-        let redBall = UIView(frame: CGRect(x: 250, y: 250, width: 100, height: 100))
-        redBall.backgroundColor = UIColor.redColor()
-        redBall.layer.cornerRadius = 50
-        
-        self.view.addSubview(redBall)
-        
-        UIView.animateWithDuration(0.5, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: { redBall.transform = CGAffineTransformConcat(CGAffineTransformMakeScale(2.0, 2.0), CGAffineTransformMakeTranslation(70.0, 0)) }, completion: nil)
-    
+        moveBall()
+        self.redBall.hidden = false
+        print("Button did toched.")
     }
 }
 
